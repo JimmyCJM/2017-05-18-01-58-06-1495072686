a1c2016a14414367fa1c133722863c5ddcff2964
JimmyCJM <chijieming@sina.com>
2017-06-24 22:29:58 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,3 +1,8 @@
-module.exports = function main() {
-  // Write your cade here
+module.exports = function main(str) {
+  var str,str1,str2,str3,sum;
+  str1=str.charAt(0);
+  str2=str.charAt(1);
+  str3=str.charAt(2);
+  sum = str1+str2+str3;
+  return sum;
 };
