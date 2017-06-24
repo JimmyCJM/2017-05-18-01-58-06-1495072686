c10cc96673dcf01ab9633f88f9b605a83bad8cd6
JimmyCJM <chijieming@sina.com>
2017-06-24 22:35:13 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,8 +1,7 @@
 module.exports = function main(str) {
-  var str,str1,str2,str3,sum;
-  str1=str.charAt(0);
-  str2=str.charAt(1);
-  str3=str.charAt(2);
-  sum = str1+str2+str3;
+  var str,sum=0;
+  for (var i=0;i<str.length;i++){
+    sum=str.charAt(i)+sum;
+  }
   return sum;
 };
